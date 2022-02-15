196b491210431903775110eee1f8bc5188000a01
Anastasiachyp <chypyhanastasia@yahoo.it>
2022-02-15 14:15:04 +0100

    Primitive Data

--- app.js
+++ app.js
@@ -13,3 +13,33 @@ var cognome;
 console.log(cognome);
 
 //Var = key word
+
+// DATI PRIMITIVI
+
+// String
+const nome = 'anastasia sta scrivendo';
+console.log(typeof nome);
+// quando scrivi 'typeof' davanti alla variabile, nella console ti dice che tipo di variabile è. 
+// ex.stringa in questo caso.
+
+// Number
+const eta = 25
+console.log(eta);
+
+// Boolean 
+const eMaggiorenne = true;
+console.log(eMaggiorenne);
+
+// Null
+const valore = null;
+console.log(typeof valore);
+// se scrivo 'typeof' davanti alla variabile di nulla, mi darà come risultato 'object'. è un piccolo bug di JavaScript.
+
+// Undefined
+let anonimo = undefined;
+console.log(anonimo);
+
+// Symbol
+const simbolo = Symbol();
+console.log(simbolo);
+// non interessa in questo momento.
